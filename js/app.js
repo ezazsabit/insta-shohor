6dd53b27a234738e76ec8b964c8604cf4578cdde
ezazsabit <sabit20.kuet@gmail.com>
2022-03-11 22:25:56 +0600

    ddd

--- js/app.js
+++ js/app.js
@@ -43,18 +43,18 @@ const switchTab = (id) => {
         document.getElementById( "posts" ).style.display = "grid";
         document.getElementById( "liked" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
-    } else if (id === "liked") {
-        document.getElementById( "liked" ).style.display = "block";
-        document.getElementById( "posts" ).style.display = "none";
-        document.getElementById( "reported" ).style.display = "none";
-
-        displayLikedPosts();
+    } else if (id === "reported") {
+      document.getElementById( "reported" ).style.display = "block";
+      document.getElementById( "posts" ).style.display = "none";
+      document.getElementById( "liked" ).style.display = "none";
+      displayReportedPosts();
     }else  {
-       
-        document.getElementById( "reported" ).style.display = "block";
-        document.getElementById( "posts" ).style.display = "none";
-        document.getElementById( "liked" ).style.display = "none";
-        displayReportedPosts();
+      document.getElementById( "liked" ).style.display = "block";
+      document.getElementById( "posts" ).style.display = "none";
+      document.getElementById( "reported" ).style.display = "none";
+
+      displayLikedPosts();
+        
     }
 };
 
